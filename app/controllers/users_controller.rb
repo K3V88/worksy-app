da983f35d3ee0044aacf76533fea95d761159949
kevin stal <kevin.stal@gmx.ch>
2025-07-16 16:37:47 +0200

    fix2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    # Permit role only in certain cases, like in the update action (if you later add that functionality)
-    if action_name == "create"
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :category, :custom_category)
-    else
-      # If you plan to allow role updates later, you can add the `role` here when updating an existing user
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :category, :custom_category)
-    end
+  params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :category, :custom_category)
   end
 end
